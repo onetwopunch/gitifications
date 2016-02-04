8f9b05f7656beb149c89af7d26cdf77c6a75ef15
Ryan Canty <jrcanty@gmail.com>
2016-02-03 16:32:41 -0800

    fixed image issue

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const electron = require('electron');
+const nativeImage = require('electron').nativeImage;
 const app = electron.app;
 const Menu = electron.Menu;
 const MenuItem = electron.MenuItem;
@@ -10,22 +11,20 @@ const Tray = electron.Tray;
 const path = require('path');
 
 let tray, quit, messenger;
+
 var open_token_window = function() {}
 app.on('ready', function() {
-  let img = path.join(__dirname, 'img', 'github.png');
-  try {
-    tray = new Tray(img);
-  } catch(e) {
-    console.log(e.stack)
-  }
+
+  let image = nativeImage.createFromPath(path.join(__dirname, 'img', 'github.png'))
+  tray = new Tray( image );
   let contextMenu = Menu.buildFromTemplate([
     { label: 'Add token', click: open_token_window},
     { label: 'Quit', click: app.quit }
   ]);
 
   // TODO: if user hasnt set token and user name, open a window to have them do that.
-  // tray.setToolTip('This is my application.');
-  // tray.setContextMenu(contextMenu);
+  tray.setToolTip('This is my application.');
+  tray.setContextMenu(contextMenu);
 
   //Set up messenger
   messenger = new Messenger();
